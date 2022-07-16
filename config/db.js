37ec29df446b78bc3014b11ba5b6b3397425b12f
guillermo <guillermoalfredorl@gmail.com>
2022-07-16 13:43:47 -0400

    se elimino PORT del objeto

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ import { createPool } from 'mysql';
 
 const pool = createPool({
   host: process.env.HOST,
-  port: process.env.PORT_DB,
   user: process.env.USER,
   password: process.env.PASSWORD_DB,
   database: process.env.DATABASE,
